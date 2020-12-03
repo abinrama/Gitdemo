110629919d9350a612af024a5e90f5190dc41e22
Abinaya <abirams94@gmail.com>
2020-12-03 18:26:09 +0530

    American guy commit

--- src/test/java/QaSelenium/Mavenjava/RestApiTest.java
+++ src/test/java/QaSelenium/Mavenjava/RestApiTest.java
@@ -9,6 +9,9 @@ public class RestApiTest {
 		System.out.println("postjira");
 		System.out.println("postjira1");
 		System.out.println("postjira2");
+		System.out.println("postjira4");
+		System.out.println("postjira5");
+		System.out.println("postjira6");
 	}
 	@Test
 	public void deletetwitter() {
